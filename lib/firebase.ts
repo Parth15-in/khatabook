1835652d026c96a4f065ed502584a24330466928
Parth15-in <parthpatel15423@gmail.com>
2026-02-13 00:24:45 +0530

    new project

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD6yo-dVgNPVGtDGk0KvPgUpn9u5-zuA_Q",
-    authDomain: "khatabook-ac6f7.firebaseapp.com",
-    projectId: "khatabook-ac6f7",
-    storageBucket: "khatabook-ac6f7.firebasestorage.app",
-    messagingSenderId: "253109706811",
-    appId: "1:253109706811:web:4a3c3f7af5186d2ebfbc1e",
-    measurementId: "G-FT7V4H7W06"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
